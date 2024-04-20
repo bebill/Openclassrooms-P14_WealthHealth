502bf0ed099342c3eab7f55f40aa4d5401c29cc7
Ben Bld <bld.prom@gmail.com>
2024-04-20 18:18:52 +0200

    chore: create form interface

---
+++ src/interfaces/formInterface.ts
@@ -0,0 +1,11 @@
+export interface Form {
+  firstName: string;
+  lastName: string;
+  street: string;
+  city: string;
+  zip: string;
+  state: string;
+  department: string;
+  startDate: string;
+  birthDate: string;
+}
