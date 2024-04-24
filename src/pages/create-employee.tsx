920e8823e9af6427e787e65c801f745dddb18682
Ben Bld <bld.prom@gmail.com>
2024-04-24 15:28:43 +0200

    chore: create "create-employee" page

---
+++ src/pages/create-employee.tsx
@@ -0,0 +1,12 @@
+import { CreateEmployeeForm } from "../components/CreateEmployeeForm";
+
+export const CreateEmployee = () => {
+  return (
+    <main>
+      <div className="create-employee-container">
+        <h2>Create Employee</h2>
+        <CreateEmployeeForm />
+      </div>
+    </main>
+  );
+};
