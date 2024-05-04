4ada44bfd6a99a0bdd96ec6a93732836facb807a
Ben Bld <bld.prom@gmail.com>
2024-05-04 16:42:09 +0200

    chore: update search functionality to include numbers

--- src/components/search/searchQuery.ts
+++ src/components/search/searchQuery.ts
@@ -1,13 +1,17 @@
 export const employeeMatchesQuery = (employee: any, query: string) => {
-  const { firstName, lastName, department, address } = employee;
-  const { street, city, state } = address;
+  const { firstName, lastName, department, address, birthDate, startDate } =
+    employee;
+  const { street, city, state, zip } = address;
   const normalizedQuery = query.toLowerCase();
   return (
     firstName.toLowerCase().includes(normalizedQuery) ||
     lastName.toLowerCase().includes(normalizedQuery) ||
+    birthDate.includes(normalizedQuery) ||
+    startDate.includes(normalizedQuery) ||
     street.toLowerCase().includes(normalizedQuery) ||
     city.toLowerCase().includes(normalizedQuery) ||
     state.toLowerCase().includes(normalizedQuery) ||
+    zip.includes(normalizedQuery) ||
     department.toLowerCase().includes(normalizedQuery)
   );
 };
